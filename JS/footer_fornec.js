850f021552f0d9de393681d1372ebbf3add0c309
Rayssa de Freitas <rayssamf@gmail.com>
2022-07-04 21:54:32 -0300

    Ajustes

--- JS/footer_fornec.js
+++ JS/footer_fornec.js
@@ -1,14 +1,20 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
-    if (window.matchMedia("(max-width:680px)").matches) {
-        if(scroll >= 2500){
+    if (window.matchMedia("(max-width:840px)").matches) {
+        if(scroll >= 2000){
             $('#footer').fadeOut();
         } else {
             $('#footer').fadeIn();
         }
-    } else if(window.matchMedia("(max-width:1200px)").matches){
-        if(scroll >= 2400){
+    } else if(window.matchMedia("(min-width:1200px)").matches){
+        if(scroll >= 1500){
+            $('#footer').fadeOut();
+        } else {
+            $('#footer').fadeIn();
+        }
+    } else if(window.matchMedia("(min-width:1060px)").matches) {
+        if(scroll >= 2150){
             $('#footer').fadeOut();
         } else {
             $('#footer').fadeIn();
